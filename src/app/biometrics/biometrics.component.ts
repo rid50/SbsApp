65f8af903a44972e02b59fceaf4fca0dc7feeb9a
rid50 <rdavidenko@gmail.com>
2021-10-08 03:28:32 +0300

    biometrics fixed

--- src/app/biometrics/biometrics.component.ts
+++ src/app/biometrics/biometrics.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Inject, OnInit } from '@angular/core';
 
 //import * as $ from 'jquery';
 
@@ -13,6 +13,41 @@ declare const $: any;
 
 export class BiometricsComponent implements AfterViewInit {
 
+  name: string
+  title: string
+  accused: string 
+  urlImg = `${this.apiUrl}/ImageHandler.ashx`;
+
+  text =  {
+      '103': {
+        name: 'Napoleon',
+        title: 'Leader of operation “Animal Farm” after defeating his cofounder',
+        accused: `False statements, misinforming his comrades, abuse of power,
+            murder, oppression of animal rights, sabotaging comrades, association with humans`
+      },
+      '105': {
+        name: 'Alexandar',
+        title: 'Pen watchman',
+        accused: 'Excessive noise pollution in domestic district, disturbance during after-hours'
+      },
+      '104': {
+        name: 'Bert',
+        title: 'Trained pick-pocket, former pet',
+        accused: `Small-scale street robbery, multiple attempts to retrieve fruits 
+            from street sellers, pick-pocketing`
+      }      
+  }
+
+  constructor(@Inject('BASE_API_URL') private apiUrl: string) { }
+
+  selectPerson(id: string): void {
+    this.name = this.text[id].name
+    this.title = this.text[id].title
+    this.accused = this.text[id].accused
+  }
+            // that = this;
+  // id ='103'
+  // urlImg = 'http://localhost:5000/ImageHandler.ashx';
 
   //constructor() { }
 
@@ -21,10 +56,66 @@ export class BiometricsComponent implements AfterViewInit {
 
 
   ngAfterViewInit(): void {
-    $(function () {
 
+    // $('#bio li').click(() => {
+    //   console.log(this.that)
+    //   console.log(this.id)
+    // })
+    // $('#bio li').click(() => {
+    //   console.log(this.that)
+    //   console.log(this.id)
+    //   onCl(this.id, this.urlImg)
+    // })
+
+    // let idImg = '';
+    // //let $wanted_picture;
+    
+    // const $wanted_picture = $('#wanted-picture');
+    // $wanted_picture.on('load', function () {
+    //   if (idImg == 'id=103')
+    //     $('#animal').html($('#pig').html())
+    //   else if (idImg == 'id=105')
+    //     $('#animal').html($('#rooster').html())
+    //   else if (idImg == 'id=104')
+    //     $('#animal').html($('#monkey').html())
+
+    //   if ($('#div_pic').css('display') == 'none')
+    //     //$('#div_pic').slideDown(500);
+    //     $('#div_pic').attr('style', 'display: inline-block !important;')
+    //   //if ($wanted_picture.parent.css('display') == 'none')
+    //   //      $wanted_picture.parent.slideDown(500);
+    // });
+
+    // function onCl(id, urlImg) {
+    //   //console.log(id)
+    //   //console.log($(this))
+    //   idImg = id;
+
+
+
+
+    //   const pic_src = $wanted_picture.prop('src');
+    //   //alert(pic_src);
+    //   if (pic_src == '') {
+    //     $wanted_picture.prop('src', urlImg + '?' + id);
+    //   } else {
+    //     //str = pic_src.substring(0, pic_src.lastIndexOf('id=')) + id;
+    //     $wanted_picture.prop('src', pic_src.substring(0, pic_src.lastIndexOf('id=')) + id);
+    //     //alert(str);
+    //   }      
+    //   //console.log(this.id)
+
+    // }
+
+    // //const $wanted_picture = $('#wanted-picture');
+
+
+
+    //$(function () {
+      // console.log(this.that)
+      // console.log(this.id)
       let idImg = '';
-      const urlImg = 'http://localhost:5000/ImageHandler.ashx';
+      // const urlImg = `${this.apiUrl}/ImageHandler.ashx`;
       let numImg = 10;
       let pic_src = '';
 
@@ -86,8 +177,8 @@ export class BiometricsComponent implements AfterViewInit {
 
       $('.wsq').on('load', function () {
         if ($('#div_wsq').css('display') == 'none')
-          //$('#div_wsq').slideDown(500);
-          $('#div_wsq').attr('style', 'display: inline-block !important;')
+          $('#div_wsq').slideDown(500);
+          //$('#div_wsq').attr('style', 'display: inline-block !important;')
         if (--numImg == 0)
           $.unblockUI();
         /*
@@ -104,8 +195,8 @@ export class BiometricsComponent implements AfterViewInit {
       })
       .on('error', function (e) {
         if ($('#div_wsq').css('display') == 'none')
-          //$('#div_wsq').slideDown(500);
-          $('#div_wsq').attr('style', 'display: inline-block !important;')
+          $('#div_wsq').slideDown(500);
+          //$('#div_wsq').attr('style', 'display: inline-block !important;')
 
         //$('#info').text(e.type);
         if (--numImg == 0)
@@ -113,14 +204,15 @@ export class BiometricsComponent implements AfterViewInit {
       });
 
       const $wanted_picture = $('#wanted-picture');
-      $wanted_picture.on('load', function () {
-        if (idImg == 'id=103')
-          $('#animal').html($('#pig').html())
-        else if (idImg == 'id=105')
-          $('#animal').html($('#rooster').html())
-        else if (idImg == 'id=104')
-          $('#animal').html($('#monkey').html())
-
+      $wanted_picture.on('load', () => {
+        // if (idImg == 'id=103')
+        //   $('#animal').html($('#pig').html())
+        // else if (idImg == 'id=105')
+        //   $('#animal').html($('#rooster').html())
+        // else if (idImg == 'id=104')
+        //   $('#animal').html($('#monkey').html())
+        //this.name = 'kuku'
+        this.selectPerson(idImg)
         if ($('#div_pic').css('display') == 'none')
           $('#div_pic').slideDown(500);
           //$('#div_pic').attr('style', 'display: inline-block !important;')
@@ -136,8 +228,8 @@ export class BiometricsComponent implements AfterViewInit {
         const $bio = $('#bio');
         $bio.css({ 'box-shadow': '2px 2px 4px #888', '-moz-box-shadow': '2px 2px 4px #888', '-webkit-box-shadow': '2px 2px 4px #888' });
         $bio.height($bio.parent().height());
-        $bio.slideDown(500);
-        //$bio.attr('style', 'display: inline-block !important;')
+        //$bio.slideDown(500);
+        $bio.attr('style', 'display: inline-block !important;')
 
         let bio_li_background;
         $('#bio li').hover(
@@ -152,9 +244,11 @@ export class BiometricsComponent implements AfterViewInit {
           }
         );
 
-        $('#bio li').click(function () {
+       $('#bio li').click(function () {
+          //console.log(this.id)
+    
           pic_src = $(this).children('img').prop('src');
-          const id = pic_src.substring(pic_src.lastIndexOf('id='));
+          const id = pic_src.substring(pic_src.lastIndexOf('id=') + 3);
           if (id == idImg)
             return;
 
@@ -174,19 +268,19 @@ export class BiometricsComponent implements AfterViewInit {
           pic_src = $wanted_picture.prop('src');
           //alert(pic_src);
           if (pic_src == '') {
-            $wanted_picture.prop('src', urlImg + '?' + id);
+            $wanted_picture.prop('src', this.urlImg + '?id=' + id);
           } else {
             //str = pic_src.substring(0, pic_src.lastIndexOf('id=')) + id;
-            $wanted_picture.prop('src', pic_src.substring(0, pic_src.lastIndexOf('id=')) + id);
+            $wanted_picture.prop('src', pic_src.substring(0, pic_src.lastIndexOf('id=')) + 'id=' + id);
             //alert(str);
           }
 
           $('.wsq').each(function () {
             pic_src = $(this).prop('src');
             if (pic_src == '')
-              $(this).prop('src', urlImg + '?wsq=' + $(this).prop('id') + '&' + id);
+              $(this).prop('src', this.urlImg + '?wsq=' + $(this).prop('id') + '&id=' + id);
             else
-              $(this).prop('src', pic_src.substring(0, pic_src.lastIndexOf('id=')) + id);
+              $(this).prop('src', pic_src.substring(0, pic_src.lastIndexOf('id=')) + 'id=' + id);
           });
 
 
@@ -198,7 +292,7 @@ export class BiometricsComponent implements AfterViewInit {
           return false;
         });
       })
-    });
+    //});
   }
 }
 
