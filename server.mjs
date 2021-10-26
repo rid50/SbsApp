cc83223171c37894fc063ea69aab62b60319e982
rid50 <rdavidenko@gmail.com>
2021-10-26 15:17:34 +0300

    heroku deploy

--- server.mjs
+++ server.mjs
@@ -1,12 +1,10 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-//import express from 'express';
-//import path from 'path';
+import express from 'express';
+import path from 'path';
 
-const path = require('path');
-const express = require('express');
+//const path = require('path');
+//const express = require('express');
 
 import { fileURLToPath } from 'url';
-//import { dirname } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
