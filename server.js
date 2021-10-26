46606b9636102aeca950f1c52c3f8cbeba4a7dad
rid50 <rdavidenko@gmail.com>
2021-10-26 14:00:27 +0300

    heroku deploy

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import path from 'path';
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+
+app.use(express.static(__dirname + '/dist/SbsApp/'));
+
+app.get('/*', (req,res,next) => {
+    res.sendFile(path.join(__dirname + '/dist/SbsApp/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
