4aa5694b364352485f88279aee628313ffb181a2
rid50 <rdavidenko@gmail.com>
2021-10-26 15:07:27 +0300

    heroku deploy

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
-import express from 'express';
-import path from 'path';
+/* eslint-disable @typescript-eslint/no-var-requires */
+//import express from 'express';
+//import path from 'path';
+
+const path = require('path');
+const express = require('express');
 
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
