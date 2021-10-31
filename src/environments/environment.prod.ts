545b689abe83ed62bacc18585616da8300870872
rid50 <rdavidenko@gmail.com>
2021-10-31 22:27:03 +0300

    heroku deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  //apiUrl: 'https://sbs-api.yaruss.co.uk/'
-  apiUrl: 'https://SbsApi/'
+  apiUrl: 'https://sbs-api.yaruss.co.uk/'
+  //apiUrl: 'https://SbsApi/'
   // apiUrl: 'http://sbs2.yaruss.co.uk/'
   // apiUrl: 'http://localhost:5000/'
   // apiUrl: 'https://localhost:44369/'
