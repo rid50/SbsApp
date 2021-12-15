44ddf2e0f0e223a263d92c48f5475a86a1571ec8
rid50 <rdavidenko@gmail.com>
2021-12-16 00:29:52 +0300

    Lets Encrypt cert installation

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://sbs-api.yaruss.co.uk/'
+  apiUrl: 'https://sbs-api.yaruss.co.uk/'
   //apiUrl: 'https://SbsApi/'
   // apiUrl: 'http://sbs2.yaruss.co.uk/'
   // apiUrl: 'http://localhost:5000/'
